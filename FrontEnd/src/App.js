0c1612289bd164a53df34deda8428bbb9f81719a
BismahKulsoom <bismah2653@gmail.com>
2024-05-08 00:10:38 +0500

    Working on Teacher Panel

---
+++ FrontEnd/src/App.js
@@ -0,0 +1,34 @@
+import logo from './logo.svg';
+import './App.css';
+import MainScreen from './Components/LandingPage/MainScreen';
+import LoginPage from './Components/LandingPage/LoginPage';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import SideBar from './Components/TeacherPanel/Sidebar';
+import Dashboard from './Components/TeacherPanel/Dashboard';
+import Courses from './Components/TeacherPanel/Courses';
+import CreateResult from './Components/TeacherPanel/CreateResult';
+import StudentTable from './Components/Utilities/StudentTable';
+import ViewResult from './Components/TeacherPanel/ViewResult';
+
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<MainScreen />} />
+          <Route path='/login' element={<LoginPage />} />
+          <Route path='/bar' element={<SideBar />} />
+          <Route path='/courses' element={<Courses />} />
+          <Route path='/teacher' element={<Dashboard />} />
+          <Route path='/student' element={<StudentTable />} />
+          <Route path='/createResult' element={<CreateResult />} />
+          <Route path='/viewResult' element={<ViewResult />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
